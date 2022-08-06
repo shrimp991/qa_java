03fa251fbb4b38091cc276874aedde961c4d7c95
Maks Kalinin <shrimp991@mail.ru>
2022-08-06 20:02:24 +0300

    Replaced Test for exception to new Class

--- src/test/java/com/example/LionHasManeTest.java
+++ src/test/java/com/example/LionHasManeTest.java
@@ -30,14 +30,4 @@ public class LionHasManeTest {
         boolean actualHasMane = lion.doesHaveMane();
         assertEquals("Value incorrect", expectedHasMane, actualHasMane);
     }
-
-    @Test (expected = Exception.class)
-    public void testConstructorException() throws Exception {
-        try {
-            Lion lion = new Lion("Оно");
-        } catch (Exception e) {
-            assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
-            throw e;
-        }
-    }
 }
